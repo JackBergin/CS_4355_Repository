2cc9abaec31b51aa90203fa4bf565f676ca446cf
“jack <jackcbergin@gmail.com>
2022-08-26 20:57:38 -0400

    ?

---
+++ Project_Work/Project_1/PROJECT_ROOT/build/_deps/catch2-subbuild/catch2-populate-prefix/tmp/catch2-populate-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "/Users/berginjack/Desktop/Github_Work/College Projects/CS_4355_Repository/Project_Work/Project_1/PROJECT_ROOT/build/_deps/catch2-src"
+  "/Users/berginjack/Desktop/Github_Work/College Projects/CS_4355_Repository/Project_Work/Project_1/PROJECT_ROOT/build/_deps/catch2-build"
+  "/Users/berginjack/Desktop/Github_Work/College Projects/CS_4355_Repository/Project_Work/Project_1/PROJECT_ROOT/build/_deps/catch2-subbuild/catch2-populate-prefix"
+  "/Users/berginjack/Desktop/Github_Work/College Projects/CS_4355_Repository/Project_Work/Project_1/PROJECT_ROOT/build/_deps/catch2-subbuild/catch2-populate-prefix/tmp"
+  "/Users/berginjack/Desktop/Github_Work/College Projects/CS_4355_Repository/Project_Work/Project_1/PROJECT_ROOT/build/_deps/catch2-subbuild/catch2-populate-prefix/src/catch2-populate-stamp"
+  "/Users/berginjack/Desktop/Github_Work/College Projects/CS_4355_Repository/Project_Work/Project_1/PROJECT_ROOT/build/_deps/catch2-subbuild/catch2-populate-prefix/src"
+  "/Users/berginjack/Desktop/Github_Work/College Projects/CS_4355_Repository/Project_Work/Project_1/PROJECT_ROOT/build/_deps/catch2-subbuild/catch2-populate-prefix/src/catch2-populate-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "/Users/berginjack/Desktop/Github_Work/College Projects/CS_4355_Repository/Project_Work/Project_1/PROJECT_ROOT/build/_deps/catch2-subbuild/catch2-populate-prefix/src/catch2-populate-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "/Users/berginjack/Desktop/Github_Work/College Projects/CS_4355_Repository/Project_Work/Project_1/PROJECT_ROOT/build/_deps/catch2-subbuild/catch2-populate-prefix/src/catch2-populate-stamp${cfgdir}") # cfgdir has leading slash
+endif()
---
+++ Project_Work/Project_1/PROJECT_ROOT/build/bin/lexparse/CMakeFiles/generate_parser.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "("
+  ")"
+  "/Users/berginjack/Desktop/Github_Work/College Projects/CS_4355_Repository/Project_Work/Project_1/PROJECT_ROOT/src/generated/CalculatorLexer.cpp"
+  "/Users/berginjack/Desktop/Github_Work/College Projects/CS_4355_Repository/Project_Work/Project_1/PROJECT_ROOT/src/generated/CalculatorLexer.h"
+  "/Users/berginjack/Desktop/Github_Work/College Projects/CS_4355_Repository/Project_Work/Project_1/PROJECT_ROOT/src/generated/CalculatorParser.cpp"
+  "/Users/berginjack/Desktop/Github_Work/College Projects/CS_4355_Repository/Project_Work/Project_1/PROJECT_ROOT/src/generated/CalculatorParser.h"
+  "/Users/berginjack/Desktop/Github_Work/College Projects/CS_4355_Repository/Project_Work/Project_1/PROJECT_ROOT/src/lexparse/parser_sources.h"
+  "CMakeFiles/generate_parser"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/generate_parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
