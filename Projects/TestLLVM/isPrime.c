e9fe993a0e146a140714023de9454301be8026dd
“jack <jackcbergin@gmail.com>
2022-10-07 17:42:44 -0400

    Whole lot of changes

---
+++ Projects/TestLLVM/isPrime.c
@@ -0,0 +1,19 @@
+int checkPrime(int number)
+{
+    int isPrime = 1;
+
+    for (int i = 2; i <= number / 2; i++)
+    {
+        if (number % i == 0)
+        {
+            isPrime = 0;
+            break;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return isPrime;
+}
